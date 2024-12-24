d52711ec653beb6e4b9a78977f6cca22baf95e10
egorkudd <egor.ku.03@gmail.com>
2024-12-24 13:27:39 +0300

    feat: add extra information command only for bot users

--- src/main/java/org/example/commands/ExtraInfoCommand.java
+++ src/main/java/org/example/commands/ExtraInfoCommand.java
@@ -1,5 +1,10 @@
 package org.example.commands;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.entities.BotUser;
+import org.example.enums.ERole;
+import org.example.exceptions.EntityNotFoundException;
+import org.example.services.BotUserService;
 import org.example.utils.MessageUtil;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
@@ -7,20 +12,30 @@ import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
+@Slf4j
 @Component
 public class ExtraInfoCommand extends BotCommand {
+    private final BotUserService botUserService;
+
     private final String MESSAGE = """
             <b>Ссылка на дополнительные материалы:</b>
             https://disk.yandex.ru/d/6i_TFMyQXOLo5g
             ️☀️ Хорошего обучения ☀️
             """;
 
-    public ExtraInfoCommand() {
+    public ExtraInfoCommand(BotUserService botUserService) {
         super("extra_info", "Extra information command");
+        this.botUserService = botUserService;
     }
 
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
-        MessageUtil.sendMessageText(chat.getId(), MESSAGE, absSender);
+        try {
+            BotUser botUser = botUserService.getByChatIdAndRole(chat.getId(), ERole.ROLE_USER);
+            MessageUtil.sendMessageText(chat.getId(), MESSAGE, absSender);
+        } catch (EntityNotFoundException e) {
+            log.error(e.getMessage());
+            MessageUtil.sendMessageText(chat.getId(), "Недостаточно прав", absSender);
+        }
     }
 }
